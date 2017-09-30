53759a0855b528bfa244b9f49f639ca285bf08e2
nikolalsvk <nikolaseap@gmail.com>
2017-09-30 18:54:31 +0000

    Mock subscribe method

--- test/pusher-js-mock-test.js
+++ test/pusher-js-mock-test.js
@@ -2,8 +2,46 @@ import { expect } from 'chai'
 import PusherMock from '../src/pusher-js-mock'
 
 describe("PusherMock", () => {
+  let pusherMock;
+
+  beforeEach(() => {
+    pusherMock = new PusherMock();
+  })
+
   it("should have channels", () => {
-    const pusherMock = new PusherMock()
-    expect(pusherMock.channels).to.be.empty
+    expect(pusherMock.channels).to.be.empty;
+  })
+
+  describe("#channel", () => {
+    it("returns instance of PusherChannelMock", () => {
+      expect(pusherMock.channel("my-channel")).to.be.an('object');
+    })
+
+    it("adds new channel to channels array", () => {
+      pusherMock.channel("my-channel");
+      expect(pusherMock.channels).to.have.all.keys("my-channel");
+    })
+
+    describe("channel is already added to channels array", () => {
+      it("returns instance of PusherChannelMock", () => {
+        expect(pusherMock.channel("my-channel")).to.be.an('object');
+      })
+
+      it("adds new channel to channels array", () => {
+        pusherMock.channel("my-channel");
+        expect(pusherMock.channels).to.have.all.keys("my-channel");
+      })
+    })
+  })
+
+  describe("#subscribe", () => {
+    it("returns instance of PusherChannelMock", () => {
+      expect(pusherMock.subscribe("my-channel")).to.be.an('object');
+    })
+
+    it("adds new channel to channels array", () => {
+      pusherMock.subscribe("my-channel");
+      expect(pusherMock.channels).to.have.all.keys("my-channel");
+    })
   })
 })
