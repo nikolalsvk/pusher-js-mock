37d6f3721c7ae47ebbeec1ffb72c3ab0b6922e11
nikolalsvk <nikolaseap@gmail.com>
2018-02-24 07:03:14 +0100

    Cover PusherFactoryMock with specs

--- src/__tests__/pusher-factory-mock-test.js
+++ src/__tests__/pusher-factory-mock-test.js
@@ -1,16 +1,22 @@
-import chai, { expect } from 'chai';
+import { expect } from 'chai';
 
-import PusherFactoryMock from '../pusher-factory-mock'
+import PusherFactoryMock from '../pusher-factory-mock';
 
 describe('PusherFactoryMock', () => {
-  let pusherFactoryMock
+  let pusherFactoryMock;
 
   beforeEach(() => {
-    const pusherKey = '19ir1pkcj13'
-    pusherFactoryMock = new PusherFactoryMock(pusherKey)
-  })
+    const pusherKey = '19ir1pkcj13';
+    pusherFactoryMock = new PusherFactoryMock(pusherKey);
+  });
 
   it('initializes pusherKey', () => {
-    expect(pusherFactoryMock.pusherKey).to.equal('19ir1pkcj13')
-  })
-})
+    expect(pusherFactoryMock.pusherKey).to.equal('19ir1pkcj13');
+  });
+
+  describe('pusherClient', () => {
+    it('return an object', () => {
+      expect(pusherFactoryMock.pusherClient()).to.be.an('object');
+    });
+  });
+});
--- src/pusher-factory-mock.js
+++ src/pusher-factory-mock.js
@@ -1,4 +1,4 @@
-import PusherMock from "./pusher-js-mock"
+import PusherMock from './pusher-js-mock';
 
 /** Class represents fake PusherFactory
  *
@@ -15,21 +15,21 @@ import PusherMock from "./pusher-js-mock"
  *
  *   pusher = pusherFactoryMock.pusherClient()
  * })
- * **/
+ */
 export default class PusherFactoryMock {
   /** Initialize PusherFactoryMock with pusherKey and sets a
    * pusherClientInstance
    * @param {String} pusherKey - Pusher app key
-   * **/
+   */
   constructor(pusherKey) {
-    this.pusherKey = pusherKey
-    this.pusherClientInstance = new PusherMock()
+    this.pusherKey = pusherKey;
+    this.pusherClientInstance = new PusherMock();
   }
 
   /** Getter for pusherClientInstance
    * @returns {PusherMock} PusherMock object that reprents pusherClient
-   * **/
+   */
   pusherClient() {
-    return this.pusherClientInstance
+    return this.pusherClientInstance;
   }
 }
