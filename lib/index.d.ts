facf38e96ebc09a6f99d53968172154fdb9f4655
Harley Alexander <harley@mayte.io>
2020-02-26 15:50:36 +1100

    🧹 remove lib from source

--- lib/index.d.ts
+++
@@ -1,5 +0,0 @@
-import PusherChannelMock from './pusher-channel-mock';
-import PusherFactoryMock from './pusher-factory-mock';
-import PusherMock from './pusher-js-mock';
-import PusherPresenceChannelMock from './pusher-presence-channel-mock';
-export { PusherMock, PusherFactoryMock, PusherChannelMock, PusherPresenceChannelMock };
